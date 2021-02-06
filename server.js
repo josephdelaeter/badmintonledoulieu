5182224bd30ec6ad828fffd736ff869843921271
jdelaeter <joseph.delaeter@gmail.com>
2021-02-06 22:33:09 +0100

    make it better

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/CVJD'));
+app.use(express.static(__dirname + '/dist/cvjd'));
 
 app.get('/*', function(req,res) {
-     res.sendFile(path.join(__dirname+'/dist/CVJD/index.html'));
+     res.sendFile(path.join(__dirname+'/dist/cvjd/index.html'));
 });
