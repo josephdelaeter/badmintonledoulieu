47666a0a33a42765eb21623867d983ec1ec95a90
jdelaeter <joseph.delaeter@gmail.com>
2021-02-06 01:11:13 +0100

    re

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const api = require('./server/routes/api');
-
-const port = 3000;
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use('/api', api);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(express.static('./dist/cvjd'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/cvjd/' }
+    );
 });
-
-app.listen(port, function() {
-    console.log('server running on localhost:' + port);
-});
+app.listen(process.env.PORT || 8080);
