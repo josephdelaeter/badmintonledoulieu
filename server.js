bfc06385e682d9893726d2e85e99e6369d90e5c4
jdelaeter <joseph.delaeter@gmail.com>
2021-02-06 16:55:54 +0100

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(__dirname + '/dist/cvjd'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/cvjd/index.html'));
+
+app.get('*', function(req,res) {
+     res.sendFile(path.join(__dirname+'/dist/cvjd/index.html'));
 });
-app.listen(process.env.PORT || 8080);
