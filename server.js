f89f4ed4220bfed610fbb484bd8bbcbfa59b8073
jdelaeter <joseph.delaeter@gmail.com>
2021-02-06 05:00:22 +0100

    re

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist'));
-app.get('/*', function (req, res) {
-
-res.sendFile(__dirname + '/dist/index.html');
-
-});
+app.use(express.static(__dirname + '/dist/cvjd'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/cvjd/index.html'));});
 app.listen(process.env.PORT || 8080);
