7a55c9097a19227ecccd34f950cfcd9c71422329
jdelaeter <joseph.delaeter@gmail.com>
2021-02-07 18:14:15 +0100

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname + '/dist/CVJD'));
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get('', function(req,res) {
-     res.sendFile(path.join(__dirname+'/dist/CVJD/index.html'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
